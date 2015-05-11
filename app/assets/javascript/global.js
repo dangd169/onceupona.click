b4f8003025532822c98f921052e1a09044513b5b
Danny of Cheese <shortieofcheese@gmail.com>
2015-05-11 13:26:46 -0400

    Add wave prototype and wave logic.

--- app/assets/javascript/global.js
+++ app/assets/javascript/global.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    $("#Option1").click(function(){
+
+     $("#Option1").click(function(){
         $("#Option1").cycle('next');
     });
 
@@ -14,5 +15,30 @@ $(document).ready(function(){
     $("#Option4").click(function(){
         $("#Option4").cycle('next');
     });
+
+    $('.wave').click(function(){
+
+            $('.wave').animate({
+                'height': '80%',
+                }, 1500);
+
+            setTimeout(function(){ $('.wave').animate({
+                'height': '20px',
+                }, 2000); }, 2000);
+
+            setTimeout(function(){ $('.cycle-slideshow').cycle('next'); }, 2000);
+
+    });
+
+    function loop() {
+    $('.wave').animate({'bottom': '80'}, {
+        duration: 1000, 
+        complete: function() {
+            $('.wave').animate({bottom: 100}, {
+                duration: 1000, 
+                complete: loop});
+        }});
+    }
+        loop();
 
 });
